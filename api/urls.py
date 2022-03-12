706509deba0190a8d93b58aebb0a6679f99af3e3
tasnuva-zaman <tasnuva.zaman@sslwireless.com>
2022-03-12 14:12:02 +0600

    router and viewset

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 # from .views import article_list_view, article_details
-from .views import ArticleListView
-from .views import ArticleDetails
+# from .views import ArticleListView
+# from .views import ArticleDetails
+from .views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
 urlpatterns = [
-    path('articles/', ArticleListView.as_view(), name='list'),
-    path('articles/<int:id>', ArticleDetails.as_view(), name='details'),
+    path('', include(router.urls)),
+    # path('articles/', ArticleListView.as_view(), name='list'),
+    # path('articles/<int:id>', ArticleDetails.as_view(), name='details'),
 ]
